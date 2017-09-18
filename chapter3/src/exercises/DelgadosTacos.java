fa96755b42812b7667f0ba7cc0a1337a9ee3b455
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-09-18 10:01:38 -0500

    save

---
+++ chapter3/src/exercises/DelgadosTacos.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.text.DecimalFormat;
+
+public class DelgadosTacos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		System.out.println("Your total with tax comes to $" + (df.format(totalWithTax))):
+	}
+	public static void menu() {
+		
+	}
+	public static void salesTax() {
+		
+	}
+	public static void tacoTruck() {
+		
+	}
+
+}
